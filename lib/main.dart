97c6c96672f1630d312a9c5ea9ab3b19e70e600f
dojzao <degojime@gmail.com>
2019-07-28 22:47:10 -0600

    Inicio de Sesión

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:appdogs/home_page.dart';
+import 'package:appdogs/signin_page.dart';
+import 'package:appdogs/sign_in_phone.dart';
 
-void main() {
-  runApp(TabBarDemo());
-}
+void main() => runApp(MyApp());
 
-class TabBarDemo extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color(0xFF3F5AA6),
-            title: Text("Title text"),
-          ),
-          bottomNavigationBar: menu(),
-          body: TabBarView(
-            children: [
-              Container(child: Icon(Icons.directions_car)),
-              Container(child: Icon(Icons.directions_transit)),
-              Container(child: Icon(Icons.directions_bike))
-            ],
-          ),
-        ),
+      title: 'Google Auth Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-
-  Widget menu() {
-    return Container(
-      color: Color(0xFF3F5AA6),
-      child: TabBar(
-        labelColor: Colors.white,
-        unselectedLabelColor: Colors.white70,
-        indicatorSize: TabBarIndicatorSize.tab,
-        indicatorPadding: EdgeInsets.all(5.0),
-        indicatorColor: Colors.blue,
-        tabs: [
-          Tab(
-            text: "Transactions",
-            icon: Icon(Icons.euro_symbol),
-          ),
-          Tab(
-            text: "Bills",
-            icon: Icon(Icons.assignment),
-          ),
-          Tab(
-            text: "Balance",
-            icon: Icon(Icons.account_balance_wallet),
-          ),
-        ],
+      home: FutureBuilder(
+        initialData: null,
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
+          if (snapshot.data == null) return PhoneSignInSection();
+          return HomePage();
+        },
       ),
     );
   }
---
+++ lib/signin_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:appdogs/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(),
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (input) {
+                  if(input.isEmpty){
+                    return 'Provide an email';
+                  }
+                },
+                decoration: InputDecoration(
+                    labelText: 'Email'
+                ),
+                onSaved: (input) => _email = input,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if(input.length < 6){
+                    return 'Longer password please';
+                  }
+                },
+                decoration: InputDecoration(
+                    labelText: 'Password'
+                ),
+                onSaved: (input) => _password = input,
+                obscureText: true,
+              ),
+              RaisedButton(
+                onPressed: signUp,
+                child: Text('Sign up'),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+
+  void signUp() async {
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      try{
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
+      }catch(e){
+        print(e.message);
+      }
+    }
+  }
+}
